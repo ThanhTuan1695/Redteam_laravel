153ffe2094dce44cd7443ffb5546399951d31d37
duydv <duydv@bap.jp>
2017-07-20 09:52:30 +0700

    migration updates Room column type

---
+++ app/Models/Rooms.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class Rooms
+ * @package App\Models
+ * @version July 19, 2017, 9:10 am UTC
+ */
+class Rooms extends Model
+{
+    use SoftDeletes;
+
+    public $table = 'rooms';
+    
+
+    protected $dates = ['deleted_at'];
+
+
+    public $fillable = [
+        'name',
+        'user_id',
+        'description',
+        'type',
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'name' => 'string',
+        'user_id' => 'integer',
+        'description' => 'string',
+        'type' => 'string'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        'name' => 'required',
+        'user_id' => 'required',
+        'description' => 'required',
+        'type' => 'required',
+    ];
+
+    
+}
---
+++ resources/views/backend/rooms/show_fields.blade.php
@@ -0,0 +1,41 @@
+<!-- Id Field -->
+<div class="form-group">
+    {!! Form::label('id', 'Id:') !!}
+    <p>{!! $rooms->id !!}</p>
+</div>
+
+<!-- Name Field -->
+<div class="form-group">
+    {!! Form::label('name', 'Name:') !!}
+    <p>{!! $rooms->name !!}</p>
+</div>
+
+<!-- User Id Field -->
+<div class="form-group">
+    {!! Form::label('user_id', 'User Id:') !!}
+    <p>{!! $rooms->user_id !!}</p>
+</div>
+
+<!-- Description Field -->
+<div class="form-group">
+    {!! Form::label('description', 'Description:') !!}
+    <p>{!! $rooms->description !!}</p>
+</div>
+
+<div class="form-group">
+    {!! Form::label('description', 'Description:') !!}
+    <p>{!! $rooms->type !!}</p>
+</div>
+
+<!-- Created At Field -->
+<div class="form-group">
+    {!! Form::label('created_at', 'Created At:') !!}
+    <p>{!! $rooms->created_at !!}</p>
+</div>
+
+<!-- Updated At Field -->
+<div class="form-group">
+    {!! Form::label('updated_at', 'Updated At:') !!}
+    <p>{!! $rooms->updated_at !!}</p>
+</div>
+

db2bb7d0dd0a45ef50e451197b889e6f6e8066e7
duydv <duydv@bap.jp>
2017-07-26 14:07:45 +0700

    add Emoji Helper and add Media Container

--- resources/views/frontend/room/chatRoom.blade.php
+++ resources/views/frontend/room/chatRoom.blade.php
@@ -1,8 +1,8 @@
 @extends('frontend.layouts.app')
 @include('frontend.layouts.sidebar')
 @section('content')
-    <div class="content" >
-        <div class="container " style="float:left; max-width:1200px">
+    <div class="content  col-lg-12 " >
+        <div>
             <div style="margin-bottom:20px">
                 <h3 style="display:inline"><span>@</span>{!! $get_room->name !!}</h3>
                 <button style="float:right" type="submit" class="btn btn-default"><a href="">Add Member</a></button>
@@ -28,6 +28,7 @@
                 <form action="" method="" id="form-sub">
                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
                     <input type="hidden" name="room_id" id = "room_id" value="{{ $id }}">
+                    <input type="button" class="display-media " name="media" value="media">
                     <textarea cols="1" rows="1" name="message" id="message-content" class="form-control" placeholder="Message" 
                     style="width:835px;float:left;resize:none;border-radius:5px"></textarea>
                     <label class="btn btn-default btn-file" style="display:inline; float:left;">
@@ -35,61 +36,80 @@
                     </label>
                     <button type="submit" class="btn">Submit</button>
                 </form>
-
-                <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
-                <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.3/socket.io.js"></script>
-                <script type = "text/javascript">
-                    $(document).ready(function() {
-                        $.ajaxSetup({
-                            headers: { 'X-CSRF-Token' : $('meta[name=_token]').attr('content') }
-                        });
-
-                        $('#form-sub').submit(function (e) {
-                            e.preventDefault();
-                            
-                            // var message = $('#message-content').val();
-                            var request = $.ajax({
-                                type: "get",
-                                url: '/public/sendmessage',
-                                data: {
-                                    'message' : $('#message-content').val(),
-                                    'room_id' : $('#room_id').val(),
-                                }
-                            });
-                            //reset input & focus
-                            document.getElementById("message-content").value = "";
-                            $("#message-content").focus();
-
-                            request.done(function (response, textStatus, jqXHR){
-                                //console.log("Response: " + response);
-                            });
-
-                            // Callback handler that will be called on failure
-                            request.fail(function (jqXHR, textStatus, errorThrown){
-                                //console.error("error");
-                            });
-
-                        });
-                    });
-
-                    var socket = io.connect('http://localhost:8890');
-                    socket.on('message', function (data) {
-                        var message = JSON.parse(data);
-                   
-                        $( "#messages" ).append( "<span><strong>"+message.user.username+" :</strong> "+message.created_at+
-                        "</span><p>"+message.content +"</p>" );
-                        
-                        //auto bottom scroll
-                        var container = $('#all_messages');
-                        container.scrollTop(container.get(0).scrollHeight);
-                        document.body.scrollTop = document.body.scrollHeight;
-                    });
-
-                </script>
             </div>
         </div>
     </div>
-    
+
+
+    <div class="col-lg-5 media flex hidden" >
+        ádasdádasdedia
+    </div>
+
+
+@section('scripts')
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.3/socket.io.js"></script>
+
+    <script type = "text/javascript">
+        $(document).ready(function() {
+            $('.display-media ').on('click', function (e) {
+                if($('.media').hasClass('hidden')){
+                    $('.media').removeClass('hidden').stop().fadeIn("slow");
+                    $('.content').removeClass('col-lg-12').addClass('flex').addClass('col-lg-7');
+                }
+                else {
+                    $('.media').addClass('hidden').stop().fadeOut("slow");
+                    $('.content').removeClass('col-lg-7').removeClass('flex').addClass('col-lg-12');
+                }
+
+            });
+            $.ajaxSetup({
+                headers: { 'X-CSRF-Token' : $('meta[name=_token]').attr('content') }
+            });
+
+            $('#form-sub').submit(function (e) {
+                e.preventDefault();
+
+                // var message = $('#message-content').val();
+                var request = $.ajax({
+                    type: "get",
+                    url: '/public/sendmessage',
+                    data: {
+                        'message' : $('#message-content').val(),
+                        'room_id' : $('#room_id').val(),
+                    }
+                });
+                //reset input & focus
+                document.getElementById("message-content").value = "";
+                $("#message-content").focus();
+
+                request.done(function (response, textStatus, jqXHR){
+                    //console.log("Response: " + response);
+                });
+
+                // Callback handler that will be called on failure
+                request.fail(function (jqXHR, textStatus, errorThrown){
+                    //console.error("error");
+                });
+
+            });
+        });
+
+        var socket = io.connect('http://localhost:8890');
+        socket.on('message', function (data) {
+            var message = JSON.parse(data);
+
+            $( "#messages" ).append( "<span><strong>"+message.user.username+" :</strong> "+message.created_at+
+                    "</span><p>"+message.content +"</p>" );
+
+            //auto bottom scroll
+            var container = $('#all_messages');
+            container.scrollTop(container.get(0).scrollHeight);
+            document.body.scrollTop = document.body.scrollHeight;
+        });
+
+    </script>
+@endsection
 @endsection
 
--- resources/views/frontend/single/chatUser.blade.php
+++ resources/views/frontend/single/chatUser.blade.php
@@ -1,23 +1,19 @@
 @extends('frontend.layouts.app')
 @include('frontend.layouts.sidebar')
 @section('content')
-    <div class="content" >
-        <div class="container " style="float:left; max-width:1300px">
-            <div style="height:610px;overflow-x: hiden;overflow-y: auto;word-wrap:break-word;" >
+    <div class="content  col-lg-12 " >
+        <div>
+            <div style="height:610px;overflow-x: hidden;overflow-y: auto;word-wrap:break-word;" >
                  <h3 style="color: blue;"><a href="">@ {{$user->username}} </a></h3>
                  <div class="user-panel">
-        
                         <ul id="messages">
                             @foreach ($mes as $message)
-
-                                <li> @if ($message->user->avatar != null && file_exists(public_path('/backend/images/upload/'.$message->user->avatar))) 
-
+                                <li> @if ($message->user->avatar != null && file_exists(public_path('/backend/images/upload/'.$message->user->avatar)))
                                         <img
                                             style="max-width: 45px;height: auto;"
                                          src="{{ url('/backend/images/upload/'.$message->user->avatar) }}" class="img-circle"
                                          alt="User Image"
                                         />
-
                                     @else
                                         <img 
                                             style="max-width: 45px;height: auto;"
@@ -27,12 +23,9 @@
                                     @endif<strong>{{$message->user->username}}: </strong>
                                     {{$message->created_at}}
                                     <p>{{$message->content}} </p>
-
                                 </li>
-
                             @endforeach
                         </ul>
-                    
                 </div>
             </div>
            
@@ -40,6 +33,7 @@
                 <form action="" id="form-sub">
                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
                     <input type="hidden" name="idCap" id="idCap" value="{{ $idCap }}">
+                    <input type="button" class="display-media " name="media" value="media">
                     <textarea cols="1" rows="1" name="message" id="message-content" class="form-control" placeholder="Message"
                     style="width:835px;float:left;resize:none;border-radius:5px"></textarea>
                     <label class="btn btn-default btn-file" style="display:inline; float:left;">
@@ -48,49 +42,63 @@
                     <input type="submit" class="btn btn-sub" value="Submit">
                 </form> 
             </div>
-            @section('scripts')
-
-            <script type="text/javascript">
-                $(function() {
-                     $('#form-sub').on('submit',function (e) {
-                        
-                        var token = $("input[name='_token']").val();
-                        var idCap = $("input[name='idCap']").val();
-                        var msg = $("#message-content").val();
-                        if (msg != '') {
-
-                            $.ajax({
-                                type: "POST",
-                                url: '/public/sendmessage',
-                                headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-                                data: {
-                                    '_token': token,
-                                    'message': msg,
-                                    'idCap': idCap,
-                                },
-                                success: function (data) {
-                                    console.log(data);
-                                    $("#message-content").val('');
-
-                                },
-                                error: function (data) {
-                                    console.log(2);
-                                }
-                            });
-                            return false;
-                        } else {
-                            alert("Please Add Message.");
-                            return false;
-                        }
-                        
-                    });
-                });
-                
-            </script>
-            @endsection
-
         </div>
     </div>
-    
+
+    <div class="col-lg-5 media flex hidden" >
+        ádasdádasdedia
+    </div>
+
+@section('scripts')
+
+    <script type="text/javascript">
+        $(function() {
+            $('.display-media ').on('click', function (e) {
+                if($('.media').hasClass('hidden')){
+                    $('.media').removeClass('hidden').stop().fadeIn("slow");
+                    $('.content').removeClass('col-lg-12').addClass('flex').addClass('col-lg-7');
+                }
+                else {
+                    $('.media').addClass('hidden').stop().fadeOut("slow");
+                    $('.content').removeClass('col-lg-7').removeClass('flex').addClass('col-lg-12');
+                }
+
+            });
+
+            $('#form-sub').on('submit',function (e) {
+                var token = $("input[name='_token']").val();
+                var idCap = $("input[name='idCap']").val();
+                var msg = $("#message-content").val();
+                if (msg != '') {
+
+                    $.ajax({
+                        type: "POST",
+                        url: '/public/sendmessage',
+                        headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+                        data: {
+                            '_token': token,
+                            'message': msg,
+                            'idCap': idCap,
+                        },
+                        success: function (data) {
+                            console.log(data);
+                            $("#message-content").val('');
+
+                        },
+                        error: function (data) {
+                            console.log(2);
+                        }
+                    });
+                    return false;
+                } else {
+                    alert("Please Add Message.");
+                    return false;
+                }
+
+            });
+        });
+
+    </script>
+@endsection
 @endsection
 
